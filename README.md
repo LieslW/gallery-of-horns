3fccb14325a88e04ec61b60b0f26a8164c6dd50c
Liesl White <lieslfwhite@gmail.com>
2022-04-11 12:10:56 -0700

    lab 01 work, creating and importing files

---
+++ README.md
@@ -0,0 +1,7 @@
+# Gallery of Horns 
+
+Resources:
+
+Icon Image: [Icons-Icons](https://icon-icons.com/)
+
+Icon-icons allows its users to download free and usable icons for their applications.  
--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import './App.css';
+import Footer from './Footer';
+import Header from './Header';
+import Main from './Main';
 
 class App extends React.Component {
   render() {
     return (
       <>
+      <Header />
+      <Main />
+      <Footer />
       </>
     )
   }
